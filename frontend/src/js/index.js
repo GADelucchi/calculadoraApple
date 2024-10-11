f44743bbb66decb64dd3195d28aebf37b78c1a4b
GADelucchi <gdelucchi@me.com>
2024-10-11 00:18:42 -0300

    Adding console.log

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -8,6 +8,8 @@ let dollarPlusEfect
 const fetchDollarBlue = async () => {
     try {
         const response = await fetch(url);
+        console.log(response);
+
         if (!response.ok) {
             throw new Error('Error al obtener el dólar blue');
         }

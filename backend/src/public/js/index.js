d8699c26c4dd7154642c3058450931457b6375eb
GADelucchi <gdelucchi@me.com>
2024-10-14 00:24:17 -0300

    Fix

--- backend/src/public/js/index.js
+++ backend/src/public/js/index.js
@@ -8,10 +8,10 @@ let dollarPlusEfect
 const fetchDollarBlue = async () => {
     try {
         const response = await fetch(url);
-        console.log(response.json);
+        console.log(response);
 
         if (!response.ok) {
-            throw new Error('Error al obtener el dólar blue');
+            throw new Error('Error al obtener el dólar blue', response);
         }
         const data = await response.json(); // Convierte la respuesta a JSON
         console.log('Dólar Blue desde el servidor:', data.dollarBlue); // Muestra el valor en la consola
